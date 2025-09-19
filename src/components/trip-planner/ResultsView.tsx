4bb3995748030dad474b0140e7f8aa64a3ac2822
Azizul Sheikh <azizulsheikh2003@gmail.com>
2025-09-19 13:04:10 +0000

    Add a background animation  (_for element <ResultsView>_)

--- src/components/trip-planner/ResultsView.tsx
+++ src/components/trip-planner/ResultsView.tsx
@@ -18,12 +18,15 @@ export default function ResultsView({ itinerary, isLoading, onRefine }: ResultsV
         <div className="w-full md:w-1/3 lg:w-1/4">
           <IntrospectionSidebar isLoading={isLoading} onRefine={onRefine} />
         </div>
-        <div className="w-full md:w-2/3 lg:w-3/4">
-          {isLoading && !itinerary ? (
-            <ItinerarySkeleton isInitial={true} />
-          ) : itinerary ? (
-             <ItineraryDisplay itinerary={itinerary} isLoading={isLoading} />
-          ) : null}
+        <div className="w-full md:w-2/3 lg:w-3/4 relative bg-animated-gradient p-6 rounded-2xl shadow-lg">
+          <div className="absolute inset-0 bg-white/80 dark:bg-black/80 backdrop-blur-sm rounded-2xl"></div>
+          <div className="relative">
+            {isLoading && !itinerary ? (
+              <ItinerarySkeleton isInitial={true} />
+            ) : itinerary ? (
+              <ItineraryDisplay itinerary={itinerary} isLoading={isLoading} />
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
